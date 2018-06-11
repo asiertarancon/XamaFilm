6c4e7bb298eb2e1fe3ca753924c9b1e86b2a9bfe
Asier <asiertarancon@outlook.com>
2018-06-11 23:26:51 +0200

    First commit. Layout & Views

---
+++ 1. Layouts. Views/XamaFilm/XamaFilm/DetailFilmPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamaFilm
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DetailFilmPage : ContentPage
+	{
+		public DetailFilmPage (Models.Movie movie)
+		{
+			InitializeComponent ();
+
+            Title = movie.Title;
+            titleLabel.Text = movie.Title;
+            image.Source = movie.Poster;
+            yearLabel.Text = movie.Year;
+            genreLabel.Text = movie.Genre;
+        }
+	}
+}
